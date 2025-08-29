abccc370c3c69bf9aa3c4d854e526e6bbe0a1924
GabrielSean-13 <Whifter13@gmail.com>
2025-08-28 17:06:28 -0700

    plz merge

--- parent.py
+++ parent.py
@@ -3,7 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 L1 = pd.read_csv('Stress_Dataset.csv')
 L2 = pd.read_csv('StressLevelDataset.csv')
-df = pd.DataFrame
+
 '''
 # Alias : Question
 
@@ -30,48 +30,35 @@ Q20 : Academic and extracurricular activities conflicting for you?
 Q21 : Do you attend classes regularly?,Have you gained/lost weight?
 Q22 : Which type of stress do you primarily experience?
 '''
+#Drop the voided column since it was a duplicate 
 L1.drop('voided', axis=1, inplace=True)
 
-L1.rename(columns={
-    'Have you recently experienced stress in your life?': 'Q01',
-    'Have you noticed a rapid heartbeat or palpitations?': 'Q02',
-    'Have you been dealing with anxiety or tension recently?': 'Q03',
-    'Do you face any sleep problems or difficulties falling asleep?': 'Q04',
-    'Have you been getting headaches more often than usual?': 'Q05',
-    'Do you get irritated easily?': 'Q06',
-    'Do you have trouble concentrating on your academic tasks?': 'Q07',
-    'Have you been feeling sadness or low mood?': 'Q08',
-    'Have you been experiencing any illness or health issues?': 'Q09',
-    'Do you often feel lonely or isolated?': 'Q10',
-    'Do you feel overwhelmed with your academic workload?': 'Q11',
-    'Are you in competition with your peers, and does it affect you?': 'Q12',
-    'Do you find that your relationship often causes you stress?': 'Q13',
-    'Are you facing any difficulties with your professors or instructors?': 'Q14',
-    'Is your working environment unpleasant or stressful?': 'Q15',
-    'Do you struggle to find time for relaxation and leisure activities?': 'Q16',
-    'Is your hostel or home environment causing you difficulties?': 'Q17',
-    'Do you lack confidence in your academic performance?': 'Q18',
-    'Do you lack confidence in your choice of academic subjects?': 'Q19',
-    'Academic and extracurricular activities conflicting for you?': 'Q20',
-    'Do you attend classes regularly?': 'Q21',
-    'Have you gained/lost weight?': 'Q22',
-    'Which type of stress do you primarily experience?': 'Q23'
-}, inplace=True)
+#Create a function to rename
+def rename_columns(df):
+    for col in range(2,len(df.columns) - 1):
+        current_column = df.columns[col]
+        df.rename(columns = {current_column: f'Q{col}'}, inplace=True)
+    return df
+
+#Function for old graph so the whole block does not needed to be commented
+def graph_data(df, graph_set):
+    L2_Beta = (df['mental_health_history'], df['depression']/6, df['headache'], df['blood_pressure'], df['sleep_quality'], df['breathing_problem'], df['noise_level'], df['living_conditions'], df['safety'], df['basic_needs'], df['academic_performance'], df['study_load'], df['teacher_student_relationship'], df['future_career_concerns'], df['social_support'], df['peer_pressure'], df['extracurricular_activities'], df['bullying'], df['stress_level'])
+    graphSet = graph_set
+    match graphSet:
+        case 1: # Only shows self-reported values rated between 0 and 5
+            plt.plot(L2_Beta)
+            return  plt.show()
+        case 2: # Standardizes 0-30 to 0-5 scale by dividing anxiety_level and self_esteem by 6 (W.I.P.)
+             plt.plot(L2['anxiety_level']/6, L2['self_esteem']/6, L2_Beta)
+             return  plt.show()
+        case 3: # Shows all raw data
+             plt.plot(L2['anxiety_level'], L2['self_esteem'], L2_Beta)
+             return  plt.show()
+   
 
 
-'''
-L2_Beta = (L2['mental_health_history'], L2['depression']/6, L2['headache'], L2['blood_pressure'], L2['sleep_quality'], L2['breathing_problem'], L2['noise_level'], L2['living_conditions'], L2['safety'], L2['basic_needs'], L2['academic_performance'], L2['study_load'], L2['teacher_student_relationship'], L2['future_career_concerns'], L2['social_support'], L2['peer_pressure'], L2['extracurricular_activities'], L2['bullying'], L2['stress_level'])
-graphSet = 1
-match graphSet:
-    case 1: # Only shows self-reported values rated between 0 and 5
-        plt.plot(L2_Beta)
-    case 2: # Standardizes 0-30 to 0-5 scale by dividing anxiety_level and self_esteem by 6 (W.I.P.)
-        plt.plot(L2['anxiety_level']/6, L2['self_esteem']/6, L2_Beta)
-    case 3: # Shows all raw data
-        plt.plot(L2['anxiety_level'], L2['self_esteem'], L2_Beta)
-plt.show()
 #next I want to learn how to make this data into a histogram
-'''
+
 
 '''
 plt.plot(L1['Q01'], L1['Q02'], L1['Q03'], L1['Q04'], L1['Q05'], L1['Q06'], L1['Q07'], L1['Q08'], L1['Q09'], L1['Q10'], L1['Q11'], L1['Q12'], L1['Q13'], L1['Q14'], L1['Q15'], L1['Q16'], L1['Q17'], L1['Q18'], L1['Q19'], L1['Q20'], L1['Q21'], L1['Q22'], L1['Q23'])
@@ -79,6 +66,10 @@ plt.plot(L1['Q01'], L1['Q02'], L1['Q03'], L1['Q04'], L1['Q05'], L1['Q06'], L1['Q
 #print(L1[['Q01']])
 '''
 
-print(L2['stress_level'].max())
+#print(L2['stress_level'].max())
 
 #Test Please work 
+#Another line of comment
+#Work Oml
+L1 = rename_columns(L1)
+print(L1.head())
